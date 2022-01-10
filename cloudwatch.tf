81bd984cf68be6e94e2f854b4b2db84390162231
Sami Yaseen <sami.yaseen@tilotech.io>
2022-01-10 19:37:18 +0100

    Remove entity report metrics

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,20 +1,6 @@
 locals {
   metric_filters = [
     {
-      name           = "EntityReport"
-      pattern        = "[date, time, level=METRIC, metric_name=EntityReport, metric_value]"
-      value          = "$metric_value"
-      namespace      = local.prefix
-      log_group_name = module.lambda_assemble.lambda_cloudwatch_log_group_name
-    },
-    {
-      name           = "EntityReport"
-      pattern        = "[date, time, level=METRIC, metric_name=EntityReport, metric_value]"
-      value          = "$metric_value"
-      namespace      = local.prefix
-      log_group_name = module.lambda_disassemble.lambda_cloudwatch_log_group_name
-    },
-    {
       name           = "GraphQLErrors"
       pattern        = "[date, time, level=METRIC, metric_name=GraphQLErrors, metric_value]"
       value          = "$metric_value"
@@ -22,11 +8,6 @@ locals {
       log_group_name = module.lambda_api.lambda_cloudwatch_log_group_name
     }
   ]
-}
-
-resource "aws_cloudwatch_dashboard" "entity_report" {
-  dashboard_body = templatefile(format("%s/dashboards/entity-report.json", path.module), { NAMESPACE = local.prefix })
-  dashboard_name = "entity-report"
 }
 
 resource "aws_cloudwatch_log_metric_filter" "metric_filters" {
