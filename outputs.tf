848ea824391d423f4235fba23edec38978e94fc4
Sami Yaseen <sami.yaseen@tilotech.io>
2022-03-29 17:15:46 +0200

    Add outputs needed for custom domains

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "api_url" {
   value       = module.api_gateway.apigatewayv2_api_api_endpoint
   description = "The endpoint URL of the GraphQL API."
 }
+
+output "api_default_stage_id" {
+  value = module.api_gateway.default_apigatewayv2_stage_id
+  description = "The default stage ID of the GraphQL API."
+}
+
+output "api_id" {
+  value = module.api_gateway.apigatewayv2_api_id
+  description = "The ID of the GraphQL API."
+}
